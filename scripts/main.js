f7704303e4780cadc1be6d2f5c8c3fbb877df7b5
FES001 <firdavs7794948@gmail.com>
2024-11-28 21:00:09 +0500

    Working on it.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,35 @@
 const addLetterButton = document.getElementById('addLetterButton');
 const slForm = document.getElementById('slForm');
+let isFormOpen = false;
 
-// FORM
+// FORM VARIABLES
+
+const titleLetterInput = document.getElementById('titleLetterInput');
+const letterInput = document.getElementById('letterInput');
+const letterPasswordInput = document.getElementById('letterPasswordInput');
+const letterSaveButton = document.getElementById('letterSaveButton');
+
+
+// HANDLING THE FORM
+
+// DISPLAYING THE FORM
 
 function addLetterFormDisplay() {
-    slForm.classList.toggle('sl-form-js');
-    addLetterButton.classList.toggle('add-letter-button-js');
+    if (isFormOpen === false) {
+        slForm.classList.add('sl-form-js');
+        addLetterButton.classList.add('add-letter-button-js');
+        
+        isFormOpen = true;
+    } else {
+        slForm.classList.remove('sl-form-js');
+        addLetterButton.classList.remove('add-letter-button-js');
+        // RESETTING THE FORM
+        titleLetterInput.value = '';
+        letterInput.value = '';
+        letterPasswordInput.value = '';
+
+        isFormOpen = false;
+    };
 };
 
 // INITIALIZING BUTTONS
