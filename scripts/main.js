b102a6e0b723858004e4a42b3183ac82ca60eef5
FES001 <firdavs7794948@gmail.com>
2024-11-29 01:00:11 +0500

    Working on it.

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,10 @@ const letterInput = document.getElementById('letterInput');
 const letterPasswordInput = document.getElementById('letterPasswordInput');
 const letterSaveButton = document.getElementById('letterSaveButton');
 
+// PASSWORD FORM VARIABLES
+const spPasswordForm = document.querySelector('.sl-password-form');
+const enterPasswordInput = document.getElementById('enterPasswordInput');
+const enterPasswordButton = document.getElementById('enterPasswordButton');
 
 // SECRET LETTERS THEMSELVES VARIABLES
 const secretLettersThemselves = document.querySelector('.secret-letters-themselves');
@@ -35,7 +39,7 @@ function createSecretLetter(e) {
                     <path d="M14.5 16.5L14.5 10.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
                 </svg>
             </button>
-            <button class="secret-letter-itself-button">
+            <button class="secret-letter-itself-button secret-letter-itself-lock-unlock-button">
                 <svg class="secret-letter-itself-button-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
                     <path d="M12 16.5V14.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
                     <path d="M4.2678 18.8447C4.49268 20.515 5.87612 21.8235 7.55965 21.9009C8.97626 21.966 10.4153 22 12 22C13.5847 22 15.0237 21.966 16.4403 21.9009C18.1239 21.8235 19.5073 20.515 19.7322 18.8447C19.8789 17.7547 20 16.6376 20 15.5C20 14.3624 19.8789 13.2453 19.7322 12.1553C19.5073 10.485 18.1239 9.17649 16.4403 9.09909C15.0237 9.03397 13.5847 9 12 9C10.4153 9 8.97626 9.03397 7.55965 9.09909C5.87612 9.17649 4.49268 10.485 4.2678 12.1553C4.12104 13.2453 3.99999 14.3624 3.99999 15.5C3.99999 16.6376 4.12104 17.7547 4.2678 18.8447Z" stroke="currentColor" stroke-width="1.5" />
@@ -64,7 +68,26 @@ function createSecretLetter(e) {
     slForm.classList.remove('sl-form-js');
     addLetterButton.classList.remove('add-letter-button-js');
     isFormOpen = false;
+
+
+    // LOCK AND UNLOCK BUTTON
+    const unlockTheLetterButton = document.querySelectorAll('.secret-letter-itself-lock-unlock-button');
+
+    for (let i = 0; i < unlockTheLetterButton.length; i++) {
+        let isUnlocked = false;
+        unlockTheLetterButton[i].addEventListener('click', () => {
+            spPasswordForm.classList.add('sl-password-form-active');
+            isUnlocked = true;
+        });
+    };
 };
+
+// 
+
+spPasswordForm.addEventListener('click', () => {
+    spPasswordForm.classList.remove('sl-password-form-active');
+    isUnlocked = false;
+});
 
 // DISPLAYING THE FORM
 
