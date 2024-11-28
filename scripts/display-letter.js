b5725f9f3b086055f3ec7f63092239a384c877ae
FES001 <firdavs7794948@gmail.com>
2024-11-29 02:06:47 +0500

    Working on it.

---
+++ scripts/display-letter.js
@@ -0,0 +1,14 @@
+const displayLetterTitle = document.querySelector('.display-letter-title');
+const displayLetterItself = document.querySelector('.display-letter-itself');
+
+// GETTING DATA FROM LOCAL STORAGE
+
+function gettingDataFromLocalStorage() {
+    const letterItselfData = localStorage.getItem('letterItselfData');
+    const letterTitleData = localStorage.getItem('letterTitleData');
+
+    if (letterItselfData) {
+        displayLetterItself.textContent = letterItselfData;
+        displayLetterTitle.textContent = letterTitleData;
+    };
+};
